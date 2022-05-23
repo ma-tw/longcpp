7eb78717b2b793a886f5f97d6747e111306a6851
ma_tw <user@LAPTOP-EUHFNI9T.localdomain>
2022-05-23 21:54:31 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <string>
 
-constexpr int cHeight = 6;
-constexpr int plusHeight = 4;
-constexpr int plusUpperSpace = 1;
-const std::string cSymbol[] = {
+constexpr int C_HEIGHT = 6;
+constexpr int PLUS_HEIGHT = 4;
+constexpr int SPACE_ABOVE_PLUS = 1;
+const std::string C_SYMBOL[] = {
     "  _____ ",
     " /  ___|",
     "|  |    ",
@@ -12,7 +12,7 @@ const std::string cSymbol[] = {
     "|  |___ ",
     " \\_____|"
 };
-const std::string plusSymbol[] = {
+const std::string PLUS_SYMBOL[] = {
     "   _   ",
     " _| |_ ",
     "|_   _|",
@@ -20,11 +20,11 @@ const std::string plusSymbol[] = {
 };
 
 void printAsciiArt(int plusNum) {
-    for (int i = 0; i < cHeight; i++) {
-        std::cout << cSymbol[i];
-        if (plusUpperSpace <= i && i <= plusHeight) {
+    for (int i = 0; i < C_HEIGHT; i++) {
+        std::cout << C_SYMBOL[i];
+        if (SPACE_ABOVE_PLUS <= i && i <= PLUS_HEIGHT) {
             for (int j = 0; j < plusNum; j++) {
-                std::cout << plusSymbol[i - 1];
+                std::cout << PLUS_SYMBOL[i - 1];
             }
         }
         std::cout << std::endl;
